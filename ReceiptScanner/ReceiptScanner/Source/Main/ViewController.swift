35ca651de34089ac5751381664cfe4437087b5eb
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2019-10-18 20:47:46 +0200

    Init

---
+++ ReceiptScanner/ReceiptScanner/Source/Main/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ReceiptScanner
+//
+//  Created by Kamil Wyszomierski on 15/10/2019.
+//  Copyright © 2019 Kamil Wyszomierski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
