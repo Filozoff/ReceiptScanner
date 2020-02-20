120e2a7e38ff9e365f8193e04587d9082e50cc22
Kamil Wyszomierski <kamil.wyszomierski+1@gmail.com>
2020-02-20 21:25:52 +0100

    `CameraService` subproject.

---
+++ CameraService/CameraService/Source/Camera/CameraService.swift
@@ -0,0 +1,19 @@
+//
+//  CameraService.swift
+//  ReceiptScanner
+//
+//  Created by Kamil Wyszomierski on 19/10/2019.
+//  Copyright © 2019 Kamil Wyszomierski. All rights reserved.
+//
+
+import AVKit
+import Combine
+
+public protocol CameraService {
+	
+	var output: PassthroughSubject<Output, Never> { get }
+	
+	func setup() -> Result<AVCaptureSession, Error>
+	func startSession()
+	func stopSession()
+}
---
+++ NeuralService/NeuralService/Source/Detector/CameraOutput.swift
@@ -0,0 +1,23 @@
+//
+//  CameraOutput.swift
+//  ReceiptScanner
+//
+//  Created by Kamil Wyszomierski on 23/10/2019.
+//  Copyright © 2019 Kamil Wyszomierski. All rights reserved.
+//
+
+import CoreVideo
+import ImageIO
+
+public class CameraOutput {
+
+	public let pixelBuffer: CVImageBuffer
+	public let orientation: CGImagePropertyOrientation
+	public let intrinsics: CFTypeRef?
+
+	public init(pixelBuffer: CVImageBuffer, orientation: CGImagePropertyOrientation, intrinsics: CFTypeRef?) {
+		self.pixelBuffer = pixelBuffer
+		self.orientation = orientation
+		self.intrinsics = intrinsics
+	}
+}
